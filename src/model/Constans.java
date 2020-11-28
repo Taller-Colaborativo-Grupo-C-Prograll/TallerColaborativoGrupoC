cdc80cabd166ce51cfa0171b128ef2c2c4c2f000
JulianF-ObandoM <75135459+JulianF-ObandoM@users.noreply.github.com>
2020-11-27 19:20:41 -0500

    Update Constans.java

--- src/model/Constans.java
+++ src/model/Constans.java
@@ -16,6 +16,37 @@ public class Constans {
 
 	
 	
+	
+	/**
+	 * searchString: Busca una cadena en la cadena por defecto, y hace un conteo de cuantas veces se encuentra ésta cadena
+	 * @param cadena
+	 * @return El número de veces que se encuentra la cadena en la constante por defecto 
+	 */
+	public static int searchString(String cadena) {
+		String nombre = "";
+		int count = 0;
+		for (int i = 0; i < CONSTANS.length(); i++) {
+			char caracter = CONSTANS.charAt(i);
+			nombre = nombre + caracter;
+			if (caracter == ' ') {
+				nombre = "";
+				continue;
+			}
+			if (cadena.length() == nombre.length() && nombre.equalsIgnoreCase(cadena)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	/**
 	 * getConstans: retorna CONSTANTS
 	 * @return
